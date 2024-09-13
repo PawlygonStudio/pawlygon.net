8b89a45e23f5717520c1c9db3a6b66f566ce7170
Redd <thiagojfcardoso@gmail.com>
2024-09-13 16:10:20 -0300

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,80 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+    	extend: {
+    		fontFamily: {
+    			sans: ['Bahnschrift']
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+				backgroundBlurred: 'hsl(var(--background) / 0.7)',
+    			foregroundBlurred: 'hsl(var(--foreground) / 0.7)',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		}
+    	}
+    },
+	plugins: [
+		plugin(({ addVariant, e, postcss }) => {
+			addVariant('firefox', ({ container, separator }) => {
+				const isFirefoxRule = postcss.atRule({
+					name: 'supports',
+					params: '(-moz-appearance:none)',
+				})
+				isFirefoxRule.append(container.nodes)
+				container.append(isFirefoxRule)
+				isFirefoxRule.walkRules((rule) => {
+					rule.selector = `.${e(
+					`firefox${separator}${rule.selector.slice(1).replaceAll('\\', '')}`
+					)}`
+				})
+			})
+	  	}),
+		require("tailwindcss-animate"),],
 }

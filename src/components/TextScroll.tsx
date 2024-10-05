721cde900ed9ec806d39000f40ae4e920d63aaa2
Redd <thiagojfcardoso@gmail.com>
2024-10-04 21:03:20 -0300

    Code Cleanup and footer

---
+++ src/components/TextScroll.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import {
+	motion,
+	useScroll,
+	useSpring,
+	useTransform,
+	useMotionValue,
+	useVelocity,
+	useAnimationFrame
+} from 'framer-motion'
+
+export function TextScroll() {
+	return <div className="absolute"></div>
+}
